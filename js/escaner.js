f63b0d87e3d8aaa8dd2e036a6ae368d39b4d6efe
danytrox <danytrox18@gmail.com>
2023-09-28 23:59:56 -0300

    smurftcat version
    simplemete simple el nombre lo dice

--- js/escaner.js
+++ js/escaner.js
@@ -1,12 +1,34 @@
 const scanner = new Instascan.Scanner({ video: document.getElementById('scanner') });
+const contenedor = document.getElementById('contenedor');
+const pop = document.querySelector('.popup');
+const x = document.querySelector('miaudio')
+
 
 scanner.addListener('scan', function (contenidoQR) {
-    document.getElementById('resultado').textContent =contenidoQR;
-});
+    const resultado = document.getElementById('resultado');
+    resultado.textContent =contenidoQR;
+    pop.classList.toggle("spread");
 
+    if(contenidoQR == "SMURF CAT"){
+        alert ("smurfing")
+        var x = document.getElementById("miaudio")
+        x.play();
+        
+    }
+    
+    
+    
+    // Limpia el resultado después de unos segundos
+   setTimeout(() => {
+        resultado.textContent = '';
+        pop.classList.toggle("spread")
+    }, 5000);
+    
+});
 Instascan.Camera.getCameras().then(cameras => {
     if (cameras.length > 0) {
         scanner.start(cameras[0]);
+        
     } else {
         console.error('No se encontraron cámaras disponibles.');
     }
