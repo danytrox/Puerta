8d4f2c461f9f41171861691e29b9fa0fc2e7495c
danytrox <danytrox18@gmail.com>
2023-10-06 14:00:13 -0300

    jsonAgregado
    se agrega Json con datos yb se emprlea en el proyecto para ser leido

--- js/escaner.js
+++ js/escaner.js
@@ -1,28 +1,51 @@
 const scanner = new Instascan.Scanner({ video: document.getElementById('scanner') });
 const contenedor = document.getElementById('contenedor');
 const pop = document.querySelector('.popup');
-const x = document.querySelector('miaudio')
+const x = document.querySelector('miaudio');
+const resultado = document.getElementById('resultado');
+const ingresadosJSON = [{
+    "id" : 1,
+    "nombre" : "Danery Arriagada ",
+    "edad"   : 23,
+    "rut"    : "20451773-8"
+},
+{
+    "id" : 2,
+    "nombre" : "pepe six ",
+    "edad"   : 23,
+    "rut"    : "22451773-8"
+}
+];
+
+console.log("aaqaqa")
+
 
 
 scanner.addListener('scan', function (contenidoQR) {
-    const resultado = document.getElementById('resultado');
-    resultado.textContent =contenidoQR;
-    pop.classList.toggle("spread");
+    const qr =parseInt(contenidoQR);
 
-    if(contenidoQR == "SMURF CAT"){
-        alert ("smurfing")
-        var x = document.getElementById("miaudio")
-        x.play();
+    const findPerson = ingresadosJSON.find(persona=>persona.id === qr )
+    
+    console.log(findPerson)
+    if (findPerson) {
+        // Mostrar los datos de la persona encontrada
+        pop.classList.toggle("spread");
+        console.log("Nombre: ",findPerson.nombre,"Rut: ", findPerson.rut)
+        resultado.textContent ="Nombre: "+ findPerson.nombre + " Rut: " + findPerson.rut;
+        setTimeout(() => {
+            resultado.textContent = '';
+            pop.classList.toggle("spread") //agrega spread a la clase pop para que este aparezca
+        }, 5000);
         
+    } else {
+        alert("QR no reconocido");
+        alert(contenidoQR)
     }
     
     
     
-    // Limpia el resultado después de unos segundos
-   setTimeout(() => {
-        resultado.textContent = '';
-        pop.classList.toggle("spread")
-    }, 5000);
+     //Limpia el resultado después de unos segundos
+
     
 });
 Instascan.Camera.getCameras().then(cameras => {
